78312d31902063a78191f58a2324c373bd7b41c0
esdncalingo <esdncalingo@gmail.com>
2023-06-05 15:05:03 +0800

    change production endpoint

--- src/environment/developer.ts
+++ src/environment/developer.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'http://localhost:3000/api/v1/',
-  CABLE_URL: 'ws://localhost:3000/cable',
+  API_URL: 'https://mtg-champs-api.onrender.com/api/v1/',
+  CABLE_URL: 'wss://mtg-champs-api.onrender.com/cable',
   CLIENT_TOKEN: 'd44050c4e18a75e2814cd92a9767b9bb4d674a45'
 }
